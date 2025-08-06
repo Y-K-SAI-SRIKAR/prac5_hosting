0302f3d8c772d71ed53c391db8bff01d17761f57
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2025-08-06 08:32:49 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:6996}
+  server:{port:6996},
+  base:'prac5_hosting'
 })
