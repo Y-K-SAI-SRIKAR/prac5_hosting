06bec96d7eac1477c9202b1d744f0e907bfae3c1
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2025-08-06 08:42:10 +0530

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const BASEURL=import.meta.ev.BASE_URL;
     return (
       <>
         <header>
@@ -10,39 +11,39 @@ class App extends Component {
         </header>
         <section>
           <div className='card'>
-            <img src='/1.jpeg' alt=''/>
+            <img src='/prac5/1.jpeg' alt=''/>
             <label>PROJECT 01 </label> 
           </div>
           <div className='card'>
-            <img src='/2.jpeg' alt=''/>
+            <img src='/prac5/2.jpeg' alt=''/>
             <label>PROJECT 02 </label> 
           </div>
           <div className='card'>
-            <img src='/3.jpeg' alt=''/>
+            <img src='/prac5/3.jpeg' alt=''/>
             <label>PROJECT 03 </label> 
           </div>
           <div className='card'>
-            <img src='/4.jpeg' alt=''/>
+            <img src='/prac5/4.jpeg' alt=''/>
             <label>PROJECT 04 </label> 
           </div>
           <div className='card'>
-            <img src='/5.jpeg' alt=''/>
+            <img src='prac5/5.jpeg' alt=''/>
             <label>PROJECT 05 </label> 
           </div>
           <div className='card'>
-            <img src='/6.jpeg' alt=''/>
+            <img src='/prac5/6.jpeg' alt=''/>
             <label>PROJECT 06 </label> 
           </div>
           <div className='card'>
-            <img src='/7.jpeg' alt=''/>
+            <img src='/prac5/7.jpeg' alt=''/>
             <label>PROJECT 07 </label> 
           </div>
           <div className='card'>
-            <img src='/8.jpeg' alt=''/>
+            <img src='/prac5/8.jpeg' alt=''/>
             <label>PROJECT 08 </label> 
           </div>
           <div className='card'>
-            <img src='/9.jpeg' alt=''/>
+            <img src='/prac5/9.jpeg' alt=''/>
             <label>PROJECT 09 </label> 
           </div>
         </section>
